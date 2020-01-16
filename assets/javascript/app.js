fbcc0ed69073cadd70c2790615d98dd3ce6854b9
Carolyn Grady <owenv8@gmail.com>
2020-01-16 17:39:42 -0500

    created the trivia questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,3 +14,49 @@
     // hide all other divs and show screen after a set time
 // restart the game with out reloading the page
     // clear all divs and call for the an arrayed question
+
+function triviaArray(){
+    return{
+        'question-1':{
+            question: 'What two-word phrase occurs repeatedly throughout the episodes of Series 1 (2005) and is the title of the penultimate episode?',
+            A:'Doctor Who',
+            B:'Bad Wolf',
+            C:'Look Up',
+            D:'Dark World',
+        },
+        'question-2':{
+            question: 'What is the name of the dangerous species in the library?',
+            A:'Abzorbalof',
+            B:'Krillitane',
+            C:'Vashta Narada',
+            D:'Racnoss',
+        },
+        'question-3':{
+            question: 'Which is the doctors home world?',
+            A:'Clom',
+            B:'Raxacoricofallapatorius',
+            C:'Mars',
+            D:'Gallifrey',
+        },
+        'question-4':{
+            question: 'What pseudonym does the doctors best childhood friend go by?',
+            A:'The Master',
+            B:'The Destroyer of Worlds',
+            C:'The Professor',
+            D:'John Smith',
+        },
+        'question-5':{
+            question: 'What does T.A.R.D.I.S. stand for?',
+            A:'Travel Around Reality Doing Interesting Stuff',
+            B:'Time And Relative Dimensions In Space',
+            C:'Travel Around Relative Dimensions In Space',
+            D:'Time And Reality, Driving In Style',
+        }
+
+    }
+    
+
+    
+
+    
+}
