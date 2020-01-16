c2f914c77a0f05c28d4873044ea9f9b918422b7e
Carolyn Grady <owenv8@gmail.com>
2020-01-16 13:22:30 -0500

    pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// show one trivia question at a time
+    // pull questions and their answers/answer choices from an array object
+    // all question options respond to 'click' or right array index
+// start the timer when the question loads
+    // use setTimer method
+// if correct answer is chosen, show congrats screen and ++ answers correct
+    // hide all other divs and show congrats screen after a set time
+// if wrong answer chosen or time runs out, show correct answer, and ++ answers wrong
+    // same div shown whether wrong answer or timer runs out
+    // click function for wrong answer array index's
+// after answering phase, load next question after a few seconds
+// after all questions are through, display # of correct and wrong answers
+    // hide all other divs and show screen after a set time
+// restart the game with out reloading the page
+    // clear all divs and call for the an arrayed question
