a79ceab1b11ba8b7063bac03c459f4586f96b980
Carolyn Grady <owenv8@gmail.com>
2020-01-20 16:02:43 -0500

    update answers array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,11 +23,11 @@ $(document).ready(function () {
     ]
 
     let answer = [
-        'Bad Wolf',
-        'Vashta Narada',
-        'Gallifrey',
-        'The Master',
-        'Time And Relative Dimensions In Space'
+        'B',
+        'C',
+        'D',
+        'A',
+        'B'
     ]
 
     function startGame(){
@@ -57,17 +57,36 @@ $(document).ready(function () {
         console.log('createCard')
         // create card elements
         var question = $('<div id="question">').text(questions[index]);
-        var A = $('<button id="answer-choices" data-choice="A">').text(answerChoices[index][0]);
-        var B = $('<button id="answer-choices" data-choice="B">').text(answerChoices[index][1]);
-        var C = $('<button id="answer-choices" data-choice="C">').text(answerChoices[index][2]);
-        var D = $('<button id="answer-choices" data-choice="D">').text(answerChoices[index][3]);
+        var A = $('<button class="answer-choices" data-choice="A">').text(answerChoices[index][0]);
+        var B = $('<button class="answer-choices" data-choice="B">').text(answerChoices[index][1]);
+        var C = $('<button class="answer-choices" data-choice="C">').text(answerChoices[index][2]);
+        var D = $('<button class="answer-choices" data-choice="D">').text(answerChoices[index][3]);
 
         $('#question').append(question);
-        $('#answer-choices').append(A).append(B).append(C).append(D);
+        $('.answer-choices').append(A).append(B).append(C).append(D);
     }
 
     function answerIntake(){
+        console.log('answerIntake')
         
+        $('.answer-choices').click(function () {
+            let guess = $(this).data('choice')
+            console.log(guess)
+            console.log(answer[index])
+        
+            if (countdown === 0 || guess !== answer[index]) {
+               // wrong();
+                numWrong++;
+                console.log(numWrong)
+            }
+            else if (guess === answer[index]) {
+                //correct();
+                numCorrect++;
+                console.log(numCorrect)
+            }
+
+            //nextQuestion();
+        })
     }
 
     startGame()
