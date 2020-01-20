0743db8ef007c00f0228a9325ad35043a083e401
Carolyn Grady <owenv8@gmail.com>
2020-01-19 23:58:16 -0500

    wrote display results function

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+var numCorrect = 0;
+var numWrong = 0;
+var timer;
+
 let questions = [
-        'What two-word phrase occurs repeatedly throughout the episodes of Series 1 (2005) and is the title of the penultimate episode?',
-        'What is the name of the dangerous species in the library?',
-        'Which is the doctors home world?',
-        'What pseudonym does the doctors best childhood friend go by?',
-        'What does T.A.R.D.I.S. stand for?'
-    ]
+    'What two-word phrase occurs repeatedly throughout the episodes of Series 1 (2005) and is the title of the penultimate episode?',
+    'What is the name of the dangerous species in the library?',
+    'Which is the doctors home world?',
+    'What pseudonym does the doctors best childhood friend go by?',
+    'What does T.A.R.D.I.S. stand for?'
+]
 
 let answerChoices = [
     ['Doctor Who', 'Bad Wolf', 'Look Up', 'Dark World'],
@@ -21,40 +25,94 @@ let answer = [
     'The Master',
     'Time And Relative Dimensions In Space']
 
-    console.log(questions[1])
-    console.log(answerChoices[2][1])
-    console.log(answer[4])
-    console.log(questions.length)
+function createCard(i) {
 
-    $(document).ready(function () {
-        for (i = 0; i < questions.length; i++) {
-            createCard(i);
-            startTimer();
-            if (timer === 0 || this != answer[i]){
-                wrong();
-                numWrong++;
-            }
-            else if (this === answer[i]){
-                correct();
-                numCorrect++;
-            }
-            resetTimer();
-        }
-        displayResults();    
-    })
+    // create card elements
+    var main = $('<div id="card">');
+    var head = $('<div id="question">').text(questions[i]);
+    var A = $('<button id="answer-choices" data-choice="A">').text(answerChoices[0]);
+    var B = $('<button id="answer-choices" data-choice="B">').text(answerChoices[1]);
+    var C = $('<button id="answer-choices" data-choice="C">').text(answerChoices[2]);
+    var D = $('<button id="answer-choices" data-choice="D">').text(answerChoices[3]);
 
-    function createCard(i) {
+    // append card elements to create the question card
+    let card = main.append(head).append(A).append(B).append(C).append(D);
 
-        // create card elements
-        var main = $('<div id="card">');
-        var head = $('<div id="question">').text(questions[i]);
-        var A = $('<button id="answer-choices" data-choice="A">').text(answerChoices[0]);
-        var B = $('<button id="answer-choices" data-choice="B">').text(answerChoices[1]);
-        var C = $('<button id="answer-choices" data-choice="C">').text(answerChoices[2]);
-        var D = $('<button id="answer-choices" data-choice="D">').text(answerChoices[3]);
-    
-        // append card elements to create the question card
-        let card = main.append(head).append(A).append(B).append(C).append(D);
-    
-        $('#question-card').append(card);
+    $('#question-card').append(card);
+}
+
+function startTimer() {
+
+
+}
+
+function wrong() {
+    $('#question-card').hide();
+    $('#wrong').show();
+    // add disapoint gif
+}
+
+function correct() {
+    $('#question-card').hide();
+    $('#correct').show();
+    // add happy gif
+}
+
+function resetTimer() {
+
+}
+
+function displayResults() {
+    $('#question-card').hide();
+    $('#correct').hide();
+    $('#wrong').hide();
+    $('#timer').hide();
+    $('#final-page').hide();
+    $('#reset').show();
+
+    let correct = $('<div id="correct">').text(`You answered ${numCorrect} questions right`);
+    let wrong = $('<div id="wrong">').text(`You answered ${numWrong} questions wrong`);
+    let percent = numCorrect / questions.length;
+    if (percent >= .9) {
+        let grade = $('<div id="grade">').text('Great job! Wait... Are you the Doctor?');
     }
+    else  if (percent >= .8 ) {
+        let grade = $('<div id="grade">').text('Definetely companion material');
+    }
+    else  if (percent >= .7 ) {
+        let grade = $('<div id="grade">').text('You almost had it! Better luck next time');
+    }
+    else  if (percent >= .6 ) {
+        let grade = $('<div id="grade">').text('Not much of a Whovian I see');
+    }
+    else {
+        let grade = $('<div id="grade">').text('...ouch');
+    }
+
+    $('#final-page').append(correct).append(wrong).append(grade);
+}
+
+
+$(document).ready(function () {
+
+    $('#reset').hide();
+    $('#final-page').hide();
+
+    for (i = 0; i < questions.length; i++) {
+        createCard(i);
+        startTimer();
+        if (timer === 0 || this != answer[i]) {
+            wrong();
+            numWrong++;
+        }
+        else if (this === answer[i]) {
+            correct();
+            numCorrect++;
+        }
+        resetTimer();
+    }
+    displayResults();
+})
+
+
+
